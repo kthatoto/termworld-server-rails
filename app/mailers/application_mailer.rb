312867ec6d2c43ad305b5f6ad9e8f049f52f1594
kthatoto <kthatoto@gmail.com>
2019-06-20 22:55:43 +0900

    Send token by email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  # default from: 'from@gmail.com'
-  # layout 'mailer'
+  default from: "Termworld <#{ENV['EMAIL']}>"
+  layout 'mailer'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   scope module: :accounts do
-    get "token/:email", action: :token
+    post :token
     post :login
     post :logout
   end
