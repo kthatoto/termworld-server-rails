c7dc3eebc5f03b234a6e72932bc1b92a501d2d5f
kthatoto <kthatoto@gmail.com>
2019-06-21 15:11:49 +0900

    Add auth filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::API
+
+  def authenticate!
+    binding.pry
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+  before_action :authenticate!
+
+  def index
+    render json: { action: :index }
+  end
+
+  def create
+    render json: { action: :create }
+  end
 end
